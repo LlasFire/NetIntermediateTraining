08bfe189b9079857d69cac437350defab00de2e5
Vasili Isakau <Vasili_Isakau@epam.com>
2021-10-15 21:18:12 +0300

    Partially end fifth task.

--- Multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ Multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,15 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        private static readonly object _lock = new object();
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -17,9 +21,46 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
+            var sharedList = new List<int>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                AddItemInCollection(i, sharedList).GetAwaiter().GetResult();
+                PrintItem(sharedList).GetAwaiter().GetResult();
+            }
 
             Console.ReadLine();
+        }
+
+        private static Task AddItemInCollection(int addingValue, List<int> sharedList)
+        {
+            lock (_lock)
+            {
+                var task = new Task(() =>
+                {
+                    sharedList.Add(addingValue);
+                });
+
+                task.Start();
+
+                return task;
+            }
+        }
+
+        private static Task PrintItem(List<int> sharedList)
+        {
+            lock (_lock)
+            {
+                var task = new Task(() =>
+                {
+                    var result = string.Join(", ", sharedList);
+                    Console.WriteLine($"[{result}]");
+                });
+
+                task.Start();
+
+                return task;
+            }
         }
     }
 }
