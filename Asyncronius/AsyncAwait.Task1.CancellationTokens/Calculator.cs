9bae8c1f7a97a5ec34358f6e7ea9d5f2b4519d3d
Vasili Isakau <Vasili_Isakau@epam.com>
2021-10-26 12:49:41 +0300

    Init solution of async

---
+++ Asyncronius/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.Task1.CancellationTokens
+{
+    static class Calculator
+    {
+        public static Task<long> Calculate(int n)
+        {
+            var token = CancellationToken.None;
+            return Calculate(n, token);
+        }
+
+        public static Task<long> Calculate(int n, CancellationToken token)
+        {
+            long MakeCalculation(int input, CancellationToken cancToken)
+            {
+                long sum = 0;
+
+                for (int i = 0; i < input; i++)
+                {
+                    if (cancToken.IsCancellationRequested)
+                    {
+                        throw new InvalidOperationException("The task was cancelled");
+                    }
+
+                    sum = sum + (i + 1);
+                    Thread.Sleep(10);
+                }
+
+                return sum;
+            }
+
+            return Task.Factory.StartNew(() => MakeCalculation(n, token));
+        }
+    }
+}
